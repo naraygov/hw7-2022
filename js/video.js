737fec200335665adc69abe02fc344e20873ff18
naraygov <91427219+naraygov@users.noreply.github.com>
2023-03-31 21:07:08 -0400

    Updated play code

--- js/video.js
+++ js/video.js
@@ -9,6 +9,7 @@ window.addEventListener("load", function() {
 
 	document.getElementById("play").addEventListener("click", function() {
 		console.log("Play Video");
+		video.volume = document.getElementById("slider").value / 100;
 	   video.play()
    });
    
